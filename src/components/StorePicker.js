4dd47fcef24257c69311705fba801050e75f4c24
Ashley Dance <hello@ashleydance.co.uk>
2018-06-28 22:27:58 +0100

    Add: StorePicker component made and routes user to own store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+	myInput = React.createRef();
+
+	goToStore = event => {
+		event.preventDefault();
+		const storeName = this.myInput.value.value;
+		this.props.history.push(`/store/${storeName}`);
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
-				<input type="text" required placeholder="Enter Store Name"/>
+			<form className="store-selector" onSubmit={this.goToStore}>
+				<input
+					type="text"
+					ref={this.myInput}
+					required
+					placeholder="Enter Store Name"
+					defaultValue={ getFunName() }
+				/>
 				<button type="submit">Visit Store</button>
 			</form>
 		)
