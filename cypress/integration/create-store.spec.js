6da29d8631611115d13ae0f26e5ff58f54f66a57
Michael Leners <micleners@gmail.com>
2020-02-07 17:24:10 -0600

    Do more homepage tests

--- cypress/integration/create-store.spec.js
+++ cypress/integration/create-store.spec.js
@@ -1,13 +1,22 @@
-describe("CBOT Price Table UI", () => {
-  const localUrl = `${Cypress.env("localUrl")}`;
-  const webUrl = `${Cypress.env("webUrl")}`;
+describe('CBOT Price Table UI', () => {
+  const localUrl = `${Cypress.env('localUrl')}`;
+  const webUrl = `${Cypress.env('webUrl')}`;
 
-  describe("Navigate to COTD homepage and create store", () => {
-    it("Should load the home page", () => {
-      console.log(Cypress.env());
-      cy.visit(localUrl);
-      
-      // cy.visit("https://cypress-cotd.micleners.com/");
+  describe('Navigate to COTD homepage and create store', () => {
+    beforeEach(() => {
+      cy.visit(webUrl);
+    });
+
+    it('Should load the create store page on home', () => {
+      cy.get('.store-selector input').should('exist');
+      cy.get('button').should('contain', 'Visit Store');
+    });
+
+    it('Should have a randomly generated value loaded in the input', () => {
+      let storeName1;
+      let storeName2;
+      cy.get('.store-selector input').should('exist');
+      cy.get('button').should('contain', 'Visit Store');
     });
   });
 });
